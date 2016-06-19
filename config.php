30b0947ef498dc1ad7271a4a22601bcaf28fb1f4
Wen-Yu Chang <wen-yu@apple.com>
2016-06-18 20:30:12 -0700

    make data files

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$site_urls = [
+		"http://www.calguns.net/calgunforum/forumdisplay.php?f=332",
+		"http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=2",
+		"http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=3",
+	];
+
+?>
--- run.php
+++ run.php
@@ -1,23 +1,31 @@
 #!/usr/local/bin/php
 <?php
+include( __dir__. '/config.php');
+$input_file = __dir__. '/keywords_to_watch.txt';
+$keywords_to_watch = [];
 
-$keywords_to_watch = ['san jose', 'santa clara', 'bay area', '87', 'beretta', 'shadow', 'usp compact', 'uspc', 'glock 34', 'hk45', 'glock 19','sp-01'];
+if(file_exists($input_file)){
+	$buff = file($input_file);
+	for($row_idx = 0; $row_idx < count($buff); $row_idx++){
+		$keywords_to_watch[] = strtolower(trim($buff[$row_idx]));
+	}
+} else die("Keyword File Not Found.");
 
-$buff = `curl -s --url 'http://www.calguns.net/calgunforum/forumdisplay.php?f=332' `;
+foreach($site_urls as $url){
+	$buff = `curl -s --url '{$url}' `;
 
-//<td class="alt1" id="td_threadtitle_(\d+)" title="(.+)">\r
+	$str = preg_replace('|\s|', " ", $buff);
 
-$str = preg_replace('|\s|', " ", $buff);
+	preg_match_all('|<td class="alt1" id="td_threadtitle_(\d+)" title=([^>]+)>|i', $str, $found_rows);
 
-preg_match_all('|<td class="alt1" id="td_threadtitle_(\d+)" title=([^>]+)>|i', $str, $found_rows);
-
-if(count($found_rows)> 0){
-	for($i = 0; $i < count($found_rows[2]); $i++){
-		$wts_decoded = html_entity_decode($found_rows[2][$i]);
-		str_replace($keywords_to_watch, "AA", strtolower($wts_decoded), $replaces);
-		if($replaces > 0) {
-			$tidy_str = preg_replace('|\s+|', " ", $wts_decoded);
-			print "FOUND: {$tidy_str}\nhttp://www.calguns.net/calgunforum/showthread.php?t={$found_rows[1][$i]}\n\n";
+	if(count($found_rows)> 0){
+		for($i = 0; $i < count($found_rows[2]); $i++){
+			$wts_decoded = html_entity_decode($found_rows[2][$i]);
+			str_replace($keywords_to_watch, "AA", strtolower($wts_decoded), $replaces);
+			if($replaces > 0) {
+				$tidy_str = preg_replace('|\s+|', " ", $wts_decoded);
+				print "FOUND: {$tidy_str}\nhttp://www.calguns.net/calgunforum/showthread.php?t={$found_rows[1][$i]}\n\n";
+			}
 		}
 	}
 }
