663fd0c1c22a7c81017ae9a2f60be8f1652a1e9e
Wen-Yu Chang <wen-yu@apple.com>
2018-07-12 10:21:51 -0700

    PHP Code Sniffed

--- config.php
+++ config.php
@@ -1,10 +1,18 @@
 <?php
 
-	$site_urls = [
-//		"http://www.calguns.net/calgunforum/forumdisplay.php?f=92",
-		"http://www.calguns.net/calgunforum/forumdisplay.php?f=332",
-		"http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=2",
-		"http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=3",
-	];
+    $site_urls = [
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=332",
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=2",
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=3",
+         "http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=4",
+         "http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=5",
+         "http://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page=6",
+        "--",
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=92",
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=92&order=desc&page=2",
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=92&order=desc&page=3",
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=92&order=desc&page=4",
+        "http://www.calguns.net/calgunforum/forumdisplay.php?f=92&order=desc&page=5",
+    ];
 
 ?>
--- run.php
+++ run.php
@@ -1,6 +1,6 @@
 #!/usr/local/bin/php
 <?php
-include( __dir__. '/config.php');
+require __dir__. '/config.php';
 $include_keyword_file = __dir__. '/keywords_to_watch.txt';
 $exclude_keyword_file = __dir__. '/keywords_to_exclude.txt';
 
@@ -9,70 +9,77 @@ $DEBUG = 0;
 $keywords_to_watch = [];
 $keywords_to_skip = [];
 
-if(file_exists($include_keyword_file)){
-	$buff = file($include_keyword_file);
-	for($row_idx = 0; $row_idx < count($buff); $row_idx++){
-		$keywords_to_watch[] = strtolower(trim($buff[$row_idx]));
-	}
-} else die("Watch Keyword File Not Found.");
-
-if(file_exists($exclude_keyword_file)){
-	$buff = file($exclude_keyword_file);
-	for($row_idx = 0; $row_idx < count($buff); $row_idx++){
-		$keywords_to_skip[] = trim($buff[$row_idx]);
-	}
+if (file_exists($include_keyword_file)) {
+    $buff = file($include_keyword_file);
+    for ($row_idx = 0; $row_idx < count($buff); $row_idx++) {
+        $keywords_to_watch[] = strtolower(trim($buff[$row_idx]));
+    }
+} else {
+    die("Watch Keyword File Not Found.");
 }
 
-foreach($site_urls as $url){
-	if($url == "--") {
-		print "\n##########################################################\n\n";
-		continue;
-	}
+if (file_exists($exclude_keyword_file)) {
+    $buff = file($exclude_keyword_file);
+    for ($row_idx = 0; $row_idx < count($buff); $row_idx++) {
+        $keywords_to_skip[] = trim($buff[$row_idx]);
+    }
+}
 
- 	$found_rows = [];
- 	$found_titles = [];
- 	$title_map = [];
- 	
-	$buff = `curl -s --url '{$url}' `;
+foreach ($site_urls as $url) {
+    if ($url == "--") {
+        print "\n##########################################################\n\n";
+        continue;
+    }
 
-	$str = preg_replace('|\s|', " ", $buff);
+    $found_rows = [];
+    $found_titles = [];
+    $title_map = [];
+     
+    $buff = `curl -s --url '{$url}' `;
 
-	preg_match_all('|<td class="alt1" id="td_threadtitle_(\d+)" title=([^>]+)>|i', $str, $found_rows);
-	preg_match_all('|<a href="showthread\.php\?s=.+?&amp;t=(\d+)" id="thread_title_\d+">([^<]+?)</a>|i', $str, $found_titles);
+    $str = preg_replace('|\s|', " ", $buff);
 
-	// Create a map for the post titles
-	
-	foreach($found_titles[1] as $idx => $f){
-		$title_map[$f] = $found_titles[2][$idx];
-	}
-	
-	if(count($found_rows)> 0){
-		for($i = 0; $i < count($found_rows[2]); $i++){
+    preg_match_all('|<td class="alt1" id="td_threadtitle_(\d+)" title=([^>]+)>|i', $str, $found_rows);
+    preg_match_all('|<a href="showthread\.php\?s=.+?&amp;t=(\d+)" id="thread_title_\d+">([^<]+?)</a>|i', $str, $found_titles);
 
-			$thread_id = $found_rows[1][$i];
-			
-			if(count($keywords_to_skip)){
-				if(isset($title_map[$thread_id])) $this_title =  $title_map[$thread_id];
-				else $this_title = "";
-				
-				$wts_decoded = html_entity_decode($this_title . " " . $found_rows[2][$i]);
-				$replaces = 0;
-				str_replace($keywords_to_skip, "AA", $wts_decoded, $replaces);
-				if($replaces > 0){
-					if($DEBUG) print "SKIPPING: {$wts_decoded}\n";
-					continue;
-				}
-			}
+    // Create a map for the post titles
+    
+    foreach ($found_titles[1] as $idx => $f) {
+        $title_map[$f] = $found_titles[2][$idx];
+    }
+    
+    if (count($found_rows)> 0) {
+        for ($i = 0; $i < count($found_rows[2]); $i++) {
 
-			$wts_decoded = html_entity_decode($this_title . " :: \n\n" . $found_rows[2][$i]);
+            $thread_id = $found_rows[1][$i];
+            
+            if (count($keywords_to_skip)) {
+                if (isset($title_map[$thread_id])) {
+                    $this_title =  $title_map[$thread_id];
+                } else {
+                    $this_title = "";
+                }
+                
+                $wts_decoded = html_entity_decode($this_title . " " . $found_rows[2][$i]);
+                $replaces = 0;
+                str_replace($keywords_to_skip, "AA", $wts_decoded, $replaces);
+                if ($replaces > 0) {
+                    if ($DEBUG) {
+                        print "SKIPPING: {$wts_decoded}\n";
+                    }
+                    continue;
+                }
+            }
 
-			$replaces = 0;
-			str_replace($keywords_to_watch, "AA", strtolower($wts_decoded), $replaces);
-			if($replaces > 0) {
-				$tidy_str = preg_replace('|\s+|', " ", $wts_decoded);
-				print "***\n\n{$tidy_str}\n\nhttp://www.calguns.net/calgunforum/showthread.php?t={$thread_id}\n\n";
-			}
-		}
-	}
+            $wts_decoded = html_entity_decode($this_title . " :: \n\n" . $found_rows[2][$i]);
+
+            $replaces = 0;
+            str_replace($keywords_to_watch, "AA", strtolower($wts_decoded), $replaces);
+            if ($replaces > 0) {
+                $tidy_str = preg_replace('|\s+|', " ", $wts_decoded);
+                print "***\n\n{$tidy_str}\n\nhttp://www.calguns.net/calgunforum/showthread.php?t={$thread_id}\n\n";
+            }
+        }
+    }
 }
 ?>
